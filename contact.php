22694b8fd884c06fe062bd2e5e822d707f62929f
Sukhvant <sukhvantsingh581998@gmail.com>
2021-02-10 11:54:34 +0530

    Added contact.php file

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = $_REQUEST['Name'];
+$email = $_REQUEST['Email'];
+$message = $_REQUEST['Message'];
+
+if (empty($name) || empty($email) || empty($message))
+{
+    echo "Please fill all the fields";
+}
+else
+{
+    mail("sukhvantsingh2@gmail.com", "Web Visitor", $message, "From: $name <$email>");
+    echo "<script type='text/javascript'>alert('Your message sent successfully');
+    window.history.log(-1)</script>";
+}
+
+?>
